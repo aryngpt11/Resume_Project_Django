ae71dd899bd3989b28891c6a1eea00566c478d72
aryngpt11 <aryan11gs@gmail.com>
2024-06-18 00:32:04 +0530

    completely made the home page of this

--- ResumeProject/ResumeProject/urls.py
+++ ResumeProject/ResumeProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import *
+from services.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('services/',services,name='services')
 ]
